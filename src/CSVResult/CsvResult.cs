656c580d0be38667c5e754f53e731e76b5b006af
Chad Tolkien <chad@sodadigital.com.au>
2014-06-16 15:38:48 +1000

    stubbing out implementation

---
+++ src/CSVResult/CsvResult.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using CsvHelper;
+
+namespace CsvResult
+{
+    public class CsvResult : ActionResult
+    {
+        public string ContentType { get; set; }
+        public IEnumerable Data { get; set; }
+
+        public CsvResult(IEnumerable data)
+        {
+            this.Data = data;
+            ContentType = "text/csv";
+        }
+
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+            if (string.IsNullOrEmpty(this.ContentType))
+            {
+                response.ContentType = "text/csv";
+            }
+            else
+            {
+                response.ContentType = this.ContentType;
+            }
+            if (this.Data != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    using (var textWriter = new StreamWriter(ms))
+                    {
+                        var csvSerializer = new CsvWriter(textWriter);
+                        csvSerializer.WriteRecords(Data); //write to the memory stream
+
+                        using (var textReader = new StreamReader(ms)) //read back from the memory stream
+                        {
+                            response.Write(textReader.ReadToEnd());
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ tests/CsvResult.Tests/Tests.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Xunit;
+
+namespace CsvResult.Tests
+{
+    public class TestController : Controller
+    {
+
+        public class TestClass
+        {
+            public string FirstName { get; set; }
+            public string Surname { get; set; }
+        }
+        
+
+        public CsvResult Index()
+        {
+
+            var data = new[]
+            {
+                new TestClass
+                {
+                    FirstName = "firstName",
+                    Surname = "Surname"
+                },
+                new TestClass
+                {
+                    FirstName = "2ndFirstName",
+                    Surname = "2ndsurname"
+                }
+            };
+
+            return new CsvResult(data);
+
+        }
+
+
+    }
+
+    public class Tests
+    {
+        [Fact]
+        public void TestControllerShouldReturnTypeOfCsvResult()
+        {
+            //arrange
+            var controller = new TestController();
+            //act
+            var result = controller.Index();
+            //assert
+            Assert.IsType<CsvResult>(result);
+        }
+
+        [Fact]
+        public void TestControllerDefaultMimeTypeShouldBeTextCsv()
+        {
+            //arrange
+            var controller = new TestController();
+            //act
+            var result = controller.Index();
+            //assert
+            Assert.Equal("text/csv", result.ContentType);
+        }
+
+        [Fact]
+        public void TestControllerShouldHaveTwoRows()
+        {
+            //arrange
+            var controller = new TestController();
+            //act
+            var result = controller.Index();
+            //assert
+            Assert.Equal(2, result.Data.OfType<TestController.TestClass>().Count());
+        }
+
+        [Fact]
+        public void TestControllerShouldHaveCorrectData()
+        {
+            //arrange
+            var controller = new TestController();
+            //act
+            var result = controller.Index();
+            //assert
+            Assert.Equal("2ndFirstName", result.Data.OfType<TestController.TestClass>().Last().FirstName);
+        }
+
+
+    }
+}
